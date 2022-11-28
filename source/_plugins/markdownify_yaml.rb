20569b0379050d5929d954e9cbea099d1a8ceafb
Denny Tek <tek.denny@gmail.com>
2022-11-28 14:02:14 -0500

    Update 2022-11-28T19:02:12.910Z

--- source/_plugins/markdownify_yaml.rb
+++
@@ -1,7 +0,0 @@
-module MarkdownifyYAML
-  def markdownify_yaml(input)
-    site = Jekyll.sites.first
-    converter = site.find_converter_instance(Jekyll::Converters::Markdown)
-    converter.convert(input)
-  end
-end
