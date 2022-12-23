5190b01f324f48617dd75b1f9b753160fc58ce47
Denny Tek <tek.denny@gmail.com>
2022-12-23 11:06:17 -0500

    Add filter tags
    Add dropdowns
    
    Add checkboxes

--- source/_js/product-list.js
+++ source/_js/product-list.js
@@ -4,13 +4,13 @@ const camelCase = require('lodash.camelcase');
 /* eslint-disable comma-dangle */
 const ProductSearch = {
   searchQueries: {},
-  resultsItems: [],
   sortOrder: 'desc',
   clearFiltersID: 'clear-filters',
   sessionsName: 'productSearchQueries',
   targetListId: 'product-list',
   listElement: 'product',
   dateEl: 'product-publication-date',
+  resultsItems: [],
   options: {
     targetList: undefined, // targetList holds Listjs instance
     allFilters: undefined,
@@ -18,12 +18,12 @@ const ProductSearch = {
       'product-title',
       'product-subtitle',
       'product-authors',
-      'product-types',
+      'product-type',
       'product-description',
       'product-law-area',
       'product-topic',
       'product-tag',
-      'product-center-authors',
+      'product-center-author',
       { name: 'product-publication-date', attr: 'data-timestamp' }
     ],
     fuzzySearch: {
@@ -43,19 +43,22 @@ const ProductSearch = {
     if (sessionStorage[this.sessionsName]) {
       this.matchSearchQueriesToSessions();
     } else {
-      this.setSearchQueryDefaults();
+      */
+    // this.setSearchQueryDefaults();
+    /*
     }
     this.matchSearchQueriesToUI();
     */
   },
   setSearchQueryDefaults() {
+    this.clearResultsItems();
     // used to reset sessions and search queries when clearing filters
     // list js will handle filtering text fields
     this.searchQueries = {
       productLawArea: ['all'], // checkbox
       productTopic: ['all'], // checkbox
       productTag: ['all'], // checkbox
-      productCenterAuthors: 'all', // dropdown
+      productCenterAuthor: 'all', // dropdown
       productType: 'all', // dropdown
       searchParams: '',
       sortOrder: 'desc'
@@ -65,11 +68,10 @@ const ProductSearch = {
     */
   },
   sortByDate(sortOrder) {
+    /* check mobile sort*/
     const dateToggles = document.querySelectorAll('.js-sort');
     this.searchQueries.sortOrder = sortOrder;
     this.targetList.sort(this.dateEl, { order: sortOrder });
-
-    // change UI -- can this move to match search queries to UI?
 
     if (sortOrder === 'desc') {
       dateToggles.forEach(dateToggle => {
@@ -82,11 +84,14 @@ const ProductSearch = {
         dateToggle.classList.remove('button__sort--descending');
       });
     }
+
+    this.setSessions();
     /* TO DO
-    sessionStorage.setItem('productSearchQueries', JSON.stringify(this.searchQueries));
+    sessionStorage.setItem(this.sessionsName, JSON.stringify(this.searchQueries));
     */
   },
   handleDateSortClick() {
+    // done
     const dateToggles = document.querySelectorAll('.js-sort');
     dateToggles.forEach(dateToggle => {
       dateToggle.addEventListener('click', () => {
@@ -100,9 +105,11 @@ const ProductSearch = {
   },
   // TEXT INPUT
   filterBySearchParams(searchParams) {
+    // done
     this.targetList.fuzzySearch(searchParams);
   },
   handleSearchParams() {
+    // done
     document.querySelector('#searchfield').addEventListener('keyup', e => {
       if (e.key === 'Enter' || e.keyCode === 13) {
         this.updateActiveSearchQueries();
@@ -111,102 +118,211 @@ const ProductSearch = {
     document.querySelector('.search-submit').addEventListener('click', e => {
       e.preventDefault();
       this.updateActiveSearchQueries();
-      // window.location.href = `/database/?q=${this.searchQueries.searchParams}`;
     });
   },
-  updateActiveSearchQueries() {
-    console.log(this.searchQueries);
-    // get dropdowns
+  // select and checkbox
+  filterByDropdownsAndCheckboxes() {
+    /**
+     * Dropdown filter tag ids: 'product-center-author', 'product-type'
+     * Checkbox filter tag ids: 'product-law-area', 'product-topic', 'product-tag'
+     */
 
-    /* TK
+    // camelcase dropdown filter ids (not dropdown el)
+    const { productCenterAuthor, productType } = this.searchQueries;
+
+    this.targetList.filter(
+      item =>
+        // dropdown filter ids using kebabcase
+        item
+          .values()
+          ['product-center-author'].split(' ')
+          .indexOf(productCenterAuthor) !== -1 &&
+        item
+          .values()
+          ['product-type'].split(' ')
+          .indexOf(productType) !== -1 &&
+        // include checkboxes
+        this.matchesAllItems(item.values()['product-law-area'], this.searchQueries.productLawArea) &&
+        this.matchesAllItems(item.values()['product-topic'], this.searchQueries.productTopic) &&
+        this.matchesAllItems(item.values()['product-tag'], this.searchQueries.productTag)
+    );
+  },
+  filterByCheckboxes() {
+    /**
+     * Dropdown filter tag ids: 'product-center-author', 'product-type'
+     * Checkbox filter tag ids: 'product-law-area', 'product-topic', 'product-tag'
+     */
+    const productLawAreaCheckboxes = document.querySelectorAll('.checkbox.area-checkbox');
+    const productTopicCheckboxes = document.querySelectorAll('.checkbox.topic-checkbox');
+    const productTagCheckboxes = document.querySelectorAll('.checkbox.tag-checkbox');
+
+    /**
+     * Get all checkbox elements
+     * Create empty containers for checked items
+     * Loop through each checkbox to see if it is checked
+     * Store query
+     */
+
+    const checkedProductLawAreas = [];
+    productLawAreaCheckboxes.forEach(checkbox => {
+      if (checkbox.checked === true) {
+        checkedProductLawAreas.push(checkbox.value);
+      }
+    });
+
+    const checkedProductTopics = [];
+    productTopicCheckboxes.forEach(checkbox => {
+      if (checkbox.checked === true) {
+        checkedProductTopics.push(checkbox.value);
+      }
+    });
+
+    const checkedProductTags = [];
+    productTagCheckboxes.forEach(checkbox => {
+      if (checkbox.checked === true) {
+        checkedProductTags.push(checkbox.value);
+      }
+    });
+
+    this.searchQueries.productLawArea = checkedProductLawAreas;
+    this.searchQueries.productTopic = checkedProductTopics;
+    this.searchQueries.productTag = checkedProductTags;
+
+    this.updateActiveSearchQueries();
+    /* do we need?
+    // this.filterList(this.searchQueries);
+    */
+    this.setSessions();
+  },
+  handleDropdownChange() {
+    const allDropdowns = document.querySelectorAll('.dropdown');
+    allDropdowns.forEach(dropdown => {
+      dropdown.addEventListener('change', e => {
+        this.updateActiveSearchQueries();
+        this.setSessions();
+      });
+    });
+  },
+  handleCheckboxClick() {
+    document.querySelectorAll('.checkbox-container').forEach(checkbox =>
+      checkbox.addEventListener('change', () => {
+        this.filterByCheckboxes();
+      })
+    );
+  },
+  setSessions() {
+    sessionStorage.setItem(this.sessionsName, JSON.stringify(this.searchQueries));
+  },
+  getActiveDropdowns() {
     document.querySelectorAll('.dropdown').forEach(dropdown => {
       this.searchQueries[camelCase(dropdown.id)] = dropdown[dropdown.selectedIndex].value;
     });
+  },
+  getActiveTextSearch() {
+    const textSearch = document.querySelector('#searchfield').value;
+    this.searchQueries.searchParams = textSearch;
+  },
+  getActiveCheckboxes() {
     const selectedOptions = document.querySelectorAll('option:checked');
     selectedOptions.forEach(filter => {
       const filterParam = filter.parentElement.id;
       const filterValue = filter.value;
       this.searchQueries[filterParam] = filterValue;
     });
-    */
-    const textSearch = document.querySelector('#searchfield').value;
-    this.searchQueries.searchParams = textSearch;
+  },
+  updateActiveSearchQueries() {
+    // get dropdowns
+    this.getActiveDropdowns();
+    this.getActiveCheckboxes();
+    this.getActiveTextSearch();
     this.filterList();
+    this.setSessions();
     /* TODO
-    sessionStorage.setItem('citationSearchQueries', JSON.stringify(this.searchQueries));
+    sessionStorage.setItem(this.sessionsName, JSON.stringify(this.searchQueries));
     */
   },
   filterList() {
+    this.clearResultsItems();
+    /**
+     * Dropdown filter tag ids: 'product-center-author', 'product-type'
+     * Checkbox filter tag ids: 'product-law-area', 'product-topic', 'product-tag'
+     */
     const searchParams = document.querySelector('#searchfield').value || this.searchQueries.searchParams;
-    /* TODO
-    // themes should be an array
-    if (typeof this.searchQueries.citationThemes === 'string') {
-      this.searchQueries.citationThemes = this.searchQueries.citationThemes.split();
+
+    // checkboxes should be an array
+    if (typeof this.searchQueries.productLawArea === 'string') {
+      this.searchQueries.productLawArea = this.searchQueries.productLawArea.split();
     }
-    //tags should be an array
-    if (typeof this.searchQueries.citationTags === 'string') {
-      this.searchQueries.citationTags = this.searchQueries.citationTags.split();
+
+    if (typeof this.searchQueries.productTopic === 'string') {
+      this.searchQueries.productTopic = this.searchQueries.productTopic.split();
     }
-    */
+
+    if (typeof this.searchQueries.productTag === 'string') {
+      this.searchQueries.productTag = this.searchQueries.productTag.split();
+    }
 
     this.filterBySearchParams(searchParams);
-    /* TODO
     this.filterByDropdownsAndCheckboxes();
-    */
-   this.displayResults();
-   this.displayResultQueries();
+    this.displayResults();
+    this.displayResultQueries();
   },
   clearAllFilters() {
     this.setSearchQueryDefaults();
-
     this.targetList.search();
     this.targetList.filter();
-    /* TODO
     this.clearFormInputs();
-    */
     this.displayResults();
     this.displayResultQueries();
+    this.setSessions();
     /* TODO
-    sessionStorage.setItem('citationSearchQueries', JSON.stringify(this.searchQueries));
+    sessionStorage.setItem(this.sessionsName, JSON.stringify(this.searchQueries));
 */
   },
   handleClearAllFilters() {
+    // done
     document.getElementById(`clear-filters`).addEventListener('click', () => {
       this.clearAllFilters();
     });
   },
   // DISPLAY RESULTS
-  displayResults() {
-    const count = this.targetList.matchingItems.length;
-    const label = count === 1 ? 'result' : 'results';
-    document.getElementById('results-total').innerHTML = `Displaying ${count} ${label} `;
-  },
-  displayResultQueries() {
-    this.resultsItems = [];
+  getTextResultsQuery() {
     const { searchParams } = this.searchQueries;
     if (searchParams !== '') {
       this.resultsItems.push(searchParams);
     }
-    /* TO DO
-
-    Array.from(document.querySelectorAll('.checkbox')).forEach(checkbox => {
-      if (checkbox.checked) {
-        this.resultsItems.push(document.querySelector(`label[for=${checkbox.id}]`).innerText);
-      }
-    });
+  },
+  getDropdownResultsQuery() {
     Array.from(document.querySelectorAll('.dropdown')).map(item => {
       if (item.options.selectedIndex !== 0) {
         this.resultsItems.push(document.querySelector(`option#${item.value}`).innerText);
       }
       return this.resultsItems;
     });
-    */
+  },
+  getCheckboxResultsQuery() {
+    Array.from(document.querySelectorAll('.checkbox')).forEach(checkbox => {
+      if (checkbox.checked) {
+        this.resultsItems.push(document.querySelector(`label[for=${checkbox.id}]`).innerText);
+      }
+    });
+  },
+  displayResults() {
+    const count = this.targetList.matchingItems.length;
+    const label = count === 1 ? 'result' : 'results';
+    document.getElementById('results-total').innerHTML = `Displaying ${count} ${label} `;
+  },
+  displayResultQueries() {
+    this.getTextResultsQuery();
+    this.getCheckboxResultsQuery();
+    this.getDropdownResultsQuery();
+
     if (this.resultsItems.length > 0) {
       const updateItems = document.querySelectorAll(`.${this.listElement}`);
       const resultsMessage = document.querySelector('#results-filter-detail');
       resultsMessage.classList.add('js-show');
       if (updateItems.length === 0) {
-        resultsMessage.innerHTML = `No results for: ${this.resultsItems.join('; ')}`;
+        resultsMessage.innerHTML = `${this.resultsItems.join('; ')}`;
       } else {
         resultsMessage.innerHTML = `for ${this.resultsItems.join('; ')}`;
       }
@@ -214,12 +330,75 @@ const ProductSearch = {
       document.querySelector('#results-filter-detail').classList.remove('js-show');
     }
   },
+  clearResultsItems() {
+    this.resultsItems = [];
+  },
+  clearFormInputs() {
+    document.querySelector('.search-field').value = '';
+    document.querySelectorAll('.dropdown').forEach(dropdown => {
+      dropdown.selectedIndex = 0;
+    });
+    document.querySelectorAll('.checkbox:checked').forEach(checkbox => {
+      checkbox.checked = false;
+    });
+  },
+  // helpers
+  matchesAllItems: (listItemValues, selectedFilters) => {
+    return selectedFilters.every(filter => listItemValues.indexOf(filter) !== -1);
+  },
+  matchesSomeItems: (listItemValues, selectedFilters) => {
+    if (selectedFilters.length === 0) {
+      return true;
+    }
+    return selectedFilters.some(filter => listItemValues.indexOf(filter) !== -1);
+  },
+  filterBySessionStorage() {
+    const urlSearchParam = window.location.search.split('=')[0];
+
+    if ((sessionStorage[this.sessionsName] && urlSearchParam === '') || urlSearchParam === '?q') {
+      console.log('by sessions');
+
+      const storage = JSON.parse(sessionStorage[this.sessionsName]);
+      this.searchQueries = storage;
+
+      //  get checkbox values in sessions
+      const productLawAreaCheckboxes = document.querySelectorAll('.checkbox.area-checkbox');
+      const productTopicCheckboxes = document.querySelectorAll('.checkbox.topic-checkbox');
+      const productTagCheckboxes = document.querySelectorAll('.checkbox.tag-checkbox');
+
+      // debugger;
+      productLawAreaCheckboxes.forEach(checkbox => {
+        if (this.searchQueries.productLawArea.indexOf(checkbox.id) !== -1) {
+          checkbox.checked = true;
+        }
+      });
+      productTopicCheckboxes.forEach(checkbox => {
+        if (this.searchQueries.productTopic.indexOf(checkbox.id) !== -1) {
+          checkbox.checked = true;
+        }
+      });
+      productTagCheckboxes.forEach(checkbox => {
+        if (this.searchQueries.productTag.indexOf(checkbox.id) !== -1) {
+          checkbox.checked = true;
+        }
+      });
+      /*
+       */
+      this.displayResultQueries();
+    } else {
+      debugger;
+      console.log('by url params');
+    }
+  },
   init() {
     this.createList();
     this.setSearchQueryDefaults();
     this.sortByDate(this.sortOrder);
+    this.filterBySessionStorage();
     this.handleDateSortClick();
     this.handleSearchParams();
+    this.handleDropdownChange();
+    this.handleCheckboxClick();
     this.handleClearAllFilters();
   }
 };
