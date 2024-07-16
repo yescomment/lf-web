f90ab8b2837435a63d367d98d236c3616a334336
Denny Tek <denny@objectively.is>
2024-07-16 11:18:02 -0400

    Use gh-actions

--- gulpfile.js
+++ gulpfile.js
@@ -122,13 +122,8 @@ gulp.task('test', gulp.series('test:html', 'test:es-lint', 'test:mocha'), done =
 /* =========================================
   deploy
 ========================================= */
-gulp.task('circleci', () => gulp.src('.circleci/config.yml').pipe(gulp.dest('_site/.circleci/')));
 
-gulp.task('push-gh-main', shell.task(['git push origin main']));
-
-gulp.task('push-gh-pages', () => gulp.src('_site/**/*', { dot: true }).pipe(ghPages({ force: true })));
-
-gulp.task('deploy', gulp.series('build:prod', 'circleci', 'push-gh-main', 'push-gh-pages'));
+gulp.task('build', gulp.series('build:prod'));
 
 /* =========================================
   serve
